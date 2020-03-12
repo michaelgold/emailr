fa86356b82552cf0038f5895b36cbe4e6b605b72
Michael Gold <goldmichael@gmail.com>
2020-03-11 22:55:54 -0400

    move server and port to config.json

--- emailr.py
+++ emailr.py
@@ -8,26 +8,27 @@ from email.mime.text import MIMEText
 
 from RepeatedTimer import RepeatedTimer
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
 
 with open('config.json', 'r') as f:
     config = json.load(f)
     print(config)
+
+server = smtplib.SMTP(config['server'], config['port'])
+server.starttls()
 
 server.login(config['username'], config['password'])
 
 import os
 ip = 'ifconfig'
 
-myCmd = os.popen('ifconfig').read()
+myCmd = os.popen('ifconfig').read() 
 
-server.sendmail("bmahlbrand@gmail.com", "bmahlbrand@gmail.com", ''.join(e for e in myCmd if e.isalnum() or e.isspace() or e is '.'))
+server.sendmail("gold@nyu.edu", "gold@nyu.edu", ''.join(e for e in myCmd if e.isalnum() or e.isspace() or e is '.'))
 
 
-
-rt = RepeatedTimer(1, hello, "World") # it auto-starts, no need of rt.start()
-try:
-    sleep(5) # your long-running job goes here...
-finally:
-    rt.stop() # better in a try/finally block to make sure the program ends!
+ 
+# rt = RepeatedTimer(1, hello, "World") # it auto-starts, no need of rt.start()
+# try:
+#     sleep(5) # your long-running job goes here...
+# finally:
+#     rt.stop() # better in a try/finally block to make sure the program ends!
